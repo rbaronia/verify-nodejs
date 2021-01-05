8ee2dbb9701cb4ec51d29f5309a661ac30e7a440
Jon Harry <jonharry@uk.ibm.com>
2021-01-05 14:40:41 +0000

    Basic error handling and output format

--- logintest.js
+++ logintest.js
@@ -17,21 +17,21 @@ const adaptive = new Adaptive(adaptiveConfig);
 
 // Function to get input from user
 // Includes option to mute the echo for passwords
-function getInput(question, muted=false) {
+function getInput(question, muted = false) {
   return new Promise((resolve, reject) => {
     let rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
-		if (muted) rl.stdoutMuted = true;
+    if (muted) rl.stdoutMuted = true;
 
-		rl._writeToOutput = function _writeToOutput(stringToWrite) {
-		  if (rl.stdoutMuted)
-		    rl.output.write("\x1B[2K\x1B[200D"+rl.query);
-		  else
-		    rl.output.write(stringToWrite);
-		};
-		rl.query = question;
+    rl._writeToOutput = function _writeToOutput(stringToWrite) {
+      if (rl.stdoutMuted)
+        rl.output.write("\x1B[2K\x1B[200D" + rl.query);
+      else
+        rl.output.write(stringToWrite);
+    };
+    rl.query = question;
     rl.question(rl.query, text => {
       rl.close();
       resolve(text);
@@ -40,7 +40,7 @@ function getInput(question, muted=false) {
 }
 
 async function main() {
-  let result;
+  let firstContact;
 
   // Set hard-coded context
   // Would usually be set based on incoming HTTP request
@@ -51,30 +51,42 @@ async function main() {
   }
 
   // Initialize and perform first contact assessment
-  result = await adaptive.assessPolicy(context);
-  console.log("Initial Assessment: " + JSON.stringify(result));
+  firstContact = await adaptive.assessPolicy(context);
 
-  // Assume password authentication permitted
+  console.log("*** Initial Assessment: " + JSON.stringify(firstContact, null, 2));
 
-  // Get Username and password from user
-  let username = await getInput("Username: ");
-  let password = await getInput("Password: ",true);
+  if (firstContact.status != "deny") {
 
-  if (username) {
+    // Assume password authentication permitted
 
-    // Lookup identity sources (and get the first)
-    var idSources = await adaptive.lookupIdentitySources(context,
-      result.transactionId,
-      username);
+    // Get Username and password from user
+    let username = await getInput("Username: ");
+    let password = await getInput("Password: ", true);
 
-    // Perform password authentication and JWT exchange
-    result = await adaptive.evaluatePassword(context,
-      result.transactionId,
-      idSources[0].id,
-      username, password);
+    if (username) {
 
-    console.log("After First Factor: " + JSON.stringify(result));
-  }
+      // Lookup identity sources (and get the first)
+      var idSources = await adaptive.lookupIdentitySources(context,
+        firstContact.transactionId,
+        username);
+
+      // Perform password authentication and JWT exchange
+      let result;
+      try {
+        result = await adaptive.evaluatePassword(context,
+          firstContact.transactionId,
+          idSources[0].id,
+          username, password);
+      } catch (error) {
+        if (error.response.data) {
+          console.log(error.response.data);
+        } else console.log(error);
+      }
+
+      console.log("*** After First Factor: " + JSON.stringify(result, null, 2));
+
+    } else console.log("No Username provided. Aborting.");
+  } else console.log("Access Denied (or app credentials bad). Aborting.")
 }
 
 main();
